6e5517ad429b700d5949678dc98c8ff9b1847498
Van <negneg123123123@gmail.com>
2026-03-27 15:14:32 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { RealtimeProvider } from "./hooks/useRealtime";
 
-createRoot(document.getElementById("root")!).render(
-  <>
-    <App />
-    <RealtimeProvider />     {/* ← This activates realtime for the whole app */}
-  </>
-);
+createRoot(document.getElementById("root")!).render(<App />);
