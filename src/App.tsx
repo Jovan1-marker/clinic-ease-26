05405e41ff99cfb11d6400c509b062d3c7745d2b
Van <negneg123123123@gmail.com>
2026-03-28 17:02:04 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Index from "./pages/Index";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
@@ -19,15 +20,16 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
+      
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/reset-password" element={<ResetPassword />} />
-          <Route path="/student" element={<StudentPortal />} />
-          <Route path="/admin" element={<AdminPortal />} />
+          <Route path="/reset-password" element={<ResetPassword />} /> 
+          <Route path="/studentportal" element={<StudentPortal />} />
+          <Route path="/adminportal" element={<AdminPortal />} />    
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
